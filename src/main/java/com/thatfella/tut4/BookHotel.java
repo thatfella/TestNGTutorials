325956e89c3a45b4bbd075b8234cf347319839b6
thatfella <esalex94@mail.ru>
2019-02-02 12:21:34 +0300

    Tutorial 4 Exclude, Include + Regex

---
+++ src/main/java/com/thatfella/tut4/BookHotel.java
@@ -0,0 +1,25 @@
+package com.thatfella.tut4;
+
+import org.testng.annotations.Test;
+
+public class BookHotel {
+    @Test
+    public void BookHotel_DestinationCity() {
+
+        System.out.println("BookHotel tc 1");
+    }
+
+    @Test
+    public void BookHotel_Checkin() {
+        System.out.println("BookHotel tc 2");
+
+    }
+
+
+    @Test
+    public void BookHotel_Checkout() {
+        System.out.println("BookHotel tc 3");
+
+    }
+
+}
---
+++ src/main/java/com/thatfella/tut4/FlightStatus.java
@@ -0,0 +1,11 @@
+package com.thatfella.tut4;
+
+import org.testng.annotations.Test;
+
+public class FlightStatus {
+    @Test
+    public void FlightStatus_Case1() {
+        System.out.println("FlightStatus Case 1");
+    }
+
+}
